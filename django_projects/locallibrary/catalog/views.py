e60c190e931598b2bf7ef4f443955606623c6cfd
manuloseta <manuloseta@gmail.com>
2023-02-12 19:38:22 +0100

    part 9 finished

--- django_projects/locallibrary/catalog/views.py
+++ django_projects/locallibrary/catalog/views.py
@@ -8,6 +8,11 @@ from django.urls import reverse
 
 from catalog.forms import RenewBookForm
 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from catalog.models import Author
+
 # Create your views here.
 
 from .models import Book, Author, BookInstance, Genre
@@ -97,7 +102,7 @@ def renew_book_librarian(request, pk):
             book_instance.save()
 
             # redirect to a new URL:
-            return HttpResponseRedirect(reverse('all-borrowed'))
+            return HttpResponseRedirect(reverse('librarian-borrowed'))
 
     # If this is a GET (or any other method) create the default form.
     else:
@@ -110,3 +115,30 @@ def renew_book_librarian(request, pk):
     }
 
     return render(request, 'catalog/book_renew_librarian.html', context)
+
+class AuthorCreate(CreateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    initial = {'date_of_death': '11/06/2020'}
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = '__all__' # Not recommended (potential security issue if more fields added)
+
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
+
+
+class BookCreate(CreateView):
+    model = Book
+    fields = ['title', 'author', 'summary', 'isbn', 'genre', 'language']
+    initial = {'language': 'spanish'}
+
+class BookUpdate(UpdateView):
+    model = Book
+    fields = '__all__' # Not recommended (potential security issue if more fields added)
+
+class BookDelete(DeleteView):
+    model = Book
+    success_url = reverse_lazy('authors')
