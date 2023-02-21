243b4eeb41ab42ba3683e97cb7beb98a3f5bf1d2
manuloseta <manuloseta@gmail.com>
2023-02-21 11:47:27 +0100

    uncomment build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
-# python populate_catalog.py
-# python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('alumnodb', 'alumnodb@uam.es', 'alumnodb')"
+python populate_catalog.py
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('alumnodb', 'alumnodb@uam.es', 'alumnodb')"
 
 # IMPORTANT: comment and uncomment lines as the user needs, some will fail after first time deploying
